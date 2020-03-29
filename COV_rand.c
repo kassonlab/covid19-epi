a38732351d8f9e6fd31389c9870a1e22ea21d643
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2020-03-29 12:50:42 +0200

    Add argument to switch to using a fixced seed for the random function. Default is to NOT use a fixed seed.

--- COV_rand.c
+++ COV_rand.c
@@ -1,11 +1,37 @@
 #include <stdlib.h>
 
+#if defined(USE_GETRANDOM)
+#include <linux/random.h>
+#include <errno.h>
+#else
+#include <time.h>
+#endif
+
 #include "common.h"
+
+int use_fixed_seed = 0;
 
 /* wrap the random function so we can easily change the selected random generator */
 
 void COV_init_rand() {
-    srand48(555L);
+#if defined(USE_GETRANDOM)
+    unsigned short sv[3];
+    int ret;
+#endif
+
+    if (use_fixed_seed) {
+        srand48(555L);
+    } else {
+#if defined(USE_GETRANDOM)
+        ret = getrandom(((void *)sv, sizeof(sv), 0);
+        while (ret != sizeof(sv) && (errno == EAGAIN || errno == EINTR)) {
+            ret = getrandom(((void *)sv, sizeof(sv), 0);
+        }
+        seed48(sv);
+#else
+        srand48(time(NULL));
+#endif
+    }
 }
 
 /* Must produce a values in the [0, 1) range */
