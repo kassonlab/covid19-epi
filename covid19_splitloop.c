611d45fd78ede3b635036f379237388a3f39adf3
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2020-04-06 15:23:47 +0200

    Copy "Grow county_p as needed" commit over to covid19_splitloop.c

--- covid19_splitloop.c
+++ covid19_splitloop.c
@@ -11,6 +11,9 @@
 
 #include "common.h"
 
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+
 int typical_max_HH_sz = 7;
 
 int full_fd = 0, full_kappa = 0;
@@ -153,7 +156,8 @@ void household_lat_long(int num_households, int * HH, float * lat_city, float * 
 		HH[HH_person]=HH_count;
 		city[HH_person]=city_HH[HH_count];
 		county[HH_person]=county_HH[HH_count];
-                county_p[county[HH_person]][county_size[county[HH_person]]++] = HH_person;
+                county_size[county[HH_person]]++;
+                arrput(county_p[county[HH_person]], HH_person);
 		city_size[city[HH_person]]++;
 		per_HH_size[HH[HH_person]]++;
 		locale_count[HH_count]+=1;
@@ -196,7 +200,8 @@ void household_lat_long(int num_households, int * HH, float * lat_city, float * 
 		HH[HH_person]=HH_count;
 		city[HH_person]=city_HH[HH_count];	
 		county[HH_person]=county_HH[HH_count];	
-                county_p[county[HH_person]][county_size[county[HH_person]]++] = HH_person;
+                county_size[county[HH_person]]++;
+                arrput(county_p[county[HH_person]], HH_person);
 		city_size[city[HH_person]]++;
 		locale_count[placement]+=1;
 		per_HH_size[HH[HH_person]]++;
@@ -248,7 +253,8 @@ void household_lat_long(int num_households, int * HH, float * lat_city, float * 
 			fflush(stdout);
 			city[HH_person]=city_HH[HH[HH_person]];	
 			county[HH_person]=county_HH[HH[HH_person]];	
-                        county_p[county[HH_person]][county_size[county[HH_person]]++] = HH_person;
+                        county_size[county[HH_person]]++;
+                        arrput(county_p[county[HH_person]], HH_person);
 			city_size[city[HH_person]]++;
 			locale_count[placement]+=1;
                         per_HH_size[HH[HH_person]]++;
@@ -947,10 +953,7 @@ int main (int argc, char *argv[]) {
 	int * county;  // County of each inhabitant
 	county = (int*)calloc(population,sizeof(int));
         int **county_p; /* List of persons per county */
-        county_p = (int **)malloc(num_counties * sizeof(int *));
-        for(i=0; i < num_counties; i++) {
-            county_p[i] = (int *) malloc(population * sizeof(int)); /* AS: can be reduced to max number of persons in the most populated county */
-        }
+        county_p = (int **)calloc(num_counties, sizeof(int *));
 	int * job_status; // Type of job each person holds: 0-5 for no job, preschool, elementary school, highschool/college, job, and hospital, respectively. 	
 	job_status = (int*)calloc(population,sizeof(int));
 	int * workplace; // Workplace of each person.
@@ -1264,7 +1267,11 @@ school or workplace. */
         printf("All infections initialized\n");
         fflush(stdout);
 
-
+        for (size_t i = 0; i < num_counties; ++i) {
+            arrfree(county_p[i]);
+        }
+        free(county_p);
+        county_p = NULL;
 
 	int ** job_status_county; // Jobs per county, or city for schools
 	job_status_county = (int**)calloc(6,sizeof(int*));
