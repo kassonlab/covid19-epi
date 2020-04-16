f7b00ac383fe7e679d344404e2d5bf0dc6b5c492
Soheil <ssoheil@chalmers.se>
2020-04-16 10:05:10 +0200

    modularizing: prepare household_lat_long()

---
+++ household_lat_long.h
@@ -0,0 +1,8 @@
+#ifndef HOUSEHOLD_LAT_LONG_H__
+#define HOUSEHOLD_LAT_LONG_H__
+
+#include <stdio.h>
+
+void household_lat_long(int , int * , double * , double * , int , int * , int * , int * , int * , int * , int , double * , int** , char ** , double * , double , FILE* , int **, int *, double *, double *, double *, int **, int *, double );
+
+#endif
